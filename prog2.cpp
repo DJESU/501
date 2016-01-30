9011086aa71c95319d5d98852d9bae4e349a6cda
DJESU <jesus129739@gmail.com>
2016-01-29 18:48:21 -0600

    Create prog2.cpp

---
+++ prog2.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+int main()
+          {
+            using namespace std;
+
+            int
+            a,
+            b,
+            c=0;
+
+          cout<<"Tell me the first number (integer) ";
+          cin>>a;
+
+          cout<<"\n Tell me the second number (integer) ";
+          cin>>b;
+
+          c=(a*b);
+          cout<<"\nThe product of the numers is "<<c;
+
+          c=(a/b);
+          cout<<"\nThe devision is "<<c;
+
+          c=(a%b);
+          cout<<"\nThe remainder of the division "<<c;
+
+          return 0;
+          }
