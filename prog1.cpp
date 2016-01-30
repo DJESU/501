33567056b204c84a3fe8fb6d156a65d8a4262b3f
DJESU <jesus129739@gmail.com>
2016-01-29 18:47:50 -0600

    Create prog1.cpp

---
+++ prog1.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+int main()
+          {
+          using namespace std;
+          float
+          r,
+          h,
+          v,
+          Pi;
+
+          Pi=3.1416;
+
+          cout<<"Tell me the radius (cm) ";
+          cin>>r;
+
+          cout<<"\n Tell me height (cm) ";
+          cin>>h;
+
+          v=(Pi*(r*r))*h;
+          cout<<"\n The volume of the cylinder is "<<v<<" cm^3"
+
+          return 0;
+          }
