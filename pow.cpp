46bdd494991cb59b96427ee3f5e2b20b4bf0f453
DJESU <jesus129739@gmail.com>
2016-02-09 12:42:47 -0600

    Create pow.cpp

---
+++ pow.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+#include <cmath>
+
+int main()
+          {
+            using namespace std;
+
+            int
+            a,
+            b,
+            c;
+
+            cout <<"Tell me the first number ";
+            cin >> a;
+
+            cout<<"\nTell me the second numer ";
+           cin >> b;
+
+           c=pow(a,b);
+
+           cout<<"\nThe answer is " <<c;
+           return 0;
+
+
+         }
